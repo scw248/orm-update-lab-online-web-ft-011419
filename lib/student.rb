0680f509a894a2b2318d729837b0744e8295044e
Stephen Wright <scw248@gmail.com>
2019-02-14 20:47:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,12 +46,12 @@ class Student
   end
   
   def self.new_from_db(row)
-  new_student = self.new  
-  new_student.id = row[0]
-  new_student.name =  row[1]
-  new_student.length = row[2]
-  new_student 
-end
+    new_student = self.new  
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.length = row[2]
+    new_student 
+  end
 
 
 end
