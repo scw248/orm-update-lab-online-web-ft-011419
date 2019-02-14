33f804b207a9665cea18540f0dcd5d6396521be9
Stephen Wright <scw248@gmail.com>
2019-02-14 20:50:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,5 +67,10 @@ class Student
       self.new_from_db(row)
     end.first
   end
+  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 end
